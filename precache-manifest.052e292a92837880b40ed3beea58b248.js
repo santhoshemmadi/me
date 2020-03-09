7f59e25df5bebd91029f40cf2a2ac3daef0a9d80
Santhosh <no-reply@git.com>
2020-03-09 22:42:59 +0530

    Updates

---
+++ precache-manifest.052e292a92837880b40ed3beea58b248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36a7ab25dd3ab2f0ea4357f92f6415f4",
+    "url": "/me/static/media/Professional.36a7ab25.jpg"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "9475239001a03d1fdeea",
+    "url": "/me/static/js/main.bf017c77.chunk.js"
+  },
+  {
+    "revision": "c8c02b20e2a8f4d4dfca",
+    "url": "/me/static/js/2.8250c7af.chunk.js"
+  },
+  {
+    "revision": "9475239001a03d1fdeea",
+    "url": "/me/static/css/main.004c5ddf.chunk.css"
+  },
+  {
+    "revision": "027a73435601912a98052553a7628927",
+    "url": "/me/index.html"
+  }
+];
